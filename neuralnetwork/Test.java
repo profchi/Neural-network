5a2ffa5a95849ce61bcd740fdf09c913fb1d67b8
profchi <33318715+profchi@users.noreply.github.com>
2019-02-16 15:46:45 -0800

    Update Test.java

--- neuralnetwork/Test.java
+++ neuralnetwork/Test.java
@@ -41,52 +41,15 @@ public class Test {
                     for(int i = 0; i < outNeurons.length; ++i){
                         errorTotal += Math.pow((outNeurons[i]- xor_output[inputIndex]),2)/ 2;
                     }
-                   // System.out.println(profchi.neurons[0].getWeights()[0]);
-                    //System.out.println(profchi.neurons[1].getWeights()[0]);
-                    //System.out.println(profchi.neurons[2].getWeights()[0]);
-                   // System.out.println(profchi.neurons[3].getWeights()[2]);
-                    //System.out.println(profchi.neurons[4].getWeights()[2]);
-                    //System.out.println(profchi.neurons[5].getWeights()[2]);
-                    //System.out.println();
-                    //System.out.println(profchi.neurons[7].getWeights()[1]);
-                    //System.out.println(profchi.neurons[7].getErrorSignal());
-                    //System.out.println(profchi.neurons[7].getOutput());
+                  
 
                 }
-                //System.out.println();
-                //System.out.println(profchi.neurons[7].getOutput());
                 if (errorTotal < 0.05){
                    System.out.println(epochs);
                    break;
                 }
             }
-        }/*
-        profchi.feedInputs(xor_input, 3);
-        System.out.println(profchi.neurons[7].getOutput());
-        System.out.println(profchi.neurons[7].getErrorSignal());
-        double [] me = {1};
-        //profchi.neurons[7].updateErrorSignal(0, me);
-        System.out.println(profchi.neurons[7].getErrorSignal());
-        System.out.println(profchi.neurons[7].getWeights()[2]);
-        System.out.println(profchi.neurons[5].getOutput());
-        System.out.println(profchi.neurons[5].getErrorSignal());
-        profchi.backwardPropagation(me);
-        //profchi.neurons[5].updateErrorSignal(2, xor_output);
-        //System.out.println(Neuron.Derivative(profchi.neurons[5], profchi.neurons[5].getOutput()));
-        System.out.println(profchi.neurons[7].getErrorSignal());
-        System.out.println(profchi.neurons[5].getErrorSignal());
-        /*double [] test = profchi.neurons[11].getInputs();
-        for (int i = 0; i < test.length; ++i){
-            System.out.println(test[i]); 
         }
-        System.out.println(profchi.neurons[11].getOutput());
-        System.out.println(profchi.neurons[6].outputNeuron.get(1).getOutput());
-        System.out.println(profchi.neurons[7].numberOfOutputNeurons());
-        System.out.println(profchi.neurons[8].getOutput());
-        System.out.println(profchi.neurons[9].getOutput());
-        System.out.println(profchi.neurons[0].getOutput());
-        System.out.println(profchi.neurons[5].inputNeuron.get(2).getOutput());
-        */
     }
     
 }
